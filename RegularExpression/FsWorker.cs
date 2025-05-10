6f17041c9f7b871c1408f950ef933c4259419758
aleksandrpermakov <aleksandrpermakov105@gmail.com>
2025-05-10 21:00:42 +0500

    Добавьте файлы проекта.

---
+++ RegularExpression/FsWorker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    internal class FsWorker
+    {
+        static public string GetCurrentDir() // возвращает путь , текущего каталога
+        {
+            return Directory.GetCurrentDirectory();
+        }
+        static public string ReadAllFile(string _filename) //читаем фаил
+        {
+            string result = string.Empty;
+            try
+            {
+                using (StreamReader sr = new StreamReader(_filename))
+                {
+                    result = sr.ReadToEnd();
+                }
+            }
+            catch
+            {
+                result = "<empty>";
+            }
+            return result;
+        }
+        static public List<string> ListOfFiles(string _path) // поиск в каталоге и подкаталогах название всех файлов txt
+        {
+            List<string> result = new List<string>();
+            foreach (string filename in Directory.EnumerateFiles(_path, "*.txt", SearchOption.AllDirectories))
+            {
+                result.Add(filename);
+            }
+            return result;
+        }
+
+    }
+}
---
+++ RegularExpression/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FsWorker.GetCurrentDir());
+            foreach (var item in FsWorker.ListOfFiles(FsWorker.GetCurrentDir()))
+            {
+                Console.WriteLine(item);
+            }
+            
+
+        }
+    }
+}
