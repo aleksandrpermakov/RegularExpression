b726f840b053e69e704cb03543128ecac798fe50
aleksandrpermakov <aleksandrpermakov105@gmail.com>
2025-05-10 22:20:50 +0500

    дописана обработка - поиск со звездой и без

---
+++ RegularExpression/FindPattern.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    internal class FindPattern
+    {
+        static private int counter;
+        static public int Counter { get { return counter; } }
+        static public string Find(string input, string pattern) //input - текст который обрабатываем |  pattern - обработка(синтаксис regex)
+        {
+            int _counter = 0;
+            string result = string.Empty;
+            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+            MatchCollection matches = regex.Matches(input);
+            foreach (Match match in matches)
+            {
+                result += match.ToString() + "|";
+                _counter++;
+            }
+            //Console.WriteLine($"Всего найдено {counter} совпадений");
+            counter = _counter;
+            return result;
+        }
+    }
+}
--- RegularExpression/Program.cs
+++ RegularExpression/Program.cs
@@ -10,12 +10,25 @@ namespace RegularExpression
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(FsWorker.GetCurrentDir());
-            foreach (var item in FsWorker.ListOfFiles(FsWorker.GetCurrentDir()))
+            Console.WriteLine(FsWorker.GetCurrentDir()); // текущая директория
+            foreach (var item in FsWorker.ListOfFiles(FsWorker.GetCurrentDir())) // вывожу названия всех файлов в текущем каталоге и подкаталогах
             {
                 Console.WriteLine(item);
             }
-            
+
+            List<string> files = FsWorker.ListOfFiles(FsWorker.GetCurrentDir());
+            string pattern = @"[0-9]{3,}\S{0,1}[\*]{1}"; //со звездой
+            string pattern2 = @"[0-9]{3,}[\*\.]{0,}\s"; // вместе (со звездой и без)
+            foreach (string file in files)
+            {
+                string text = FsWorker.ReadAllFile(file);
+                Console.WriteLine(text);                              //выводим весь текст из файла
+                string tasks = FindPattern.Find(text, pattern2);    
+                Console.WriteLine(tasks);                           //выводим обработку
+                Console.Write($"Количество заданий - {FindPattern.Counter} "); // Завершение задания
+                tasks = FindPattern.Find(text, pattern);
+                Console.WriteLine($", из них со звездочкой - {FindPattern.Counter}");
+            }
 
         }
     }
